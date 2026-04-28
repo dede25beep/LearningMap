59af02e5e67577787c58533e7fa3bf01f6f2d13e
André Luiz Dos Santos <mobileazhakin@gmail.com>
2026-04-28 11:15:16 -0300

    Adicionando links

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// esperar o DOM carregar
 document.addEventListener("DOMContentLoaded", function () {
   const navLinks = document.querySelectorAll(".topics-nav a");
 
@@ -6,15 +5,15 @@ document.addEventListener("DOMContentLoaded", function () {
     link.addEventListener("click", function (e) {
       e.preventDefault();
 
-      const targetId = this.getAttribute("href");         // "#Início", "#Ferramentas", etc.
+      const targetId = this.getAttribute("href");       
       const targetSection = document.querySelector(targetId);
 
-      // esconde todas as sections
+   
       document.querySelectorAll("section[id]").forEach(section => {
         section.style.display = "none";
       });
 
-      // mostra só a clicada e rola suave
+    
       if (targetSection) {
         targetSection.style.display = "block";
         targetSection.scrollIntoView({
